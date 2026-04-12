3351e0daba5bb425ccad6aa973570dd43f403787
projetocamposdojordao <projetocamposdojordao@proton.me>
2026-04-12 00:25:38 -0300

    Update generate-articles.js

--- scripts/generate-articles.js
+++ scripts/generate-articles.js
@@ -3,6 +3,12 @@
     git config user.name "github-actions[bot]"
     git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
     git add _posts
-    git diff --staged --quiet || git commit -m "Generate Jekyll articles"
+
+    if git diff --staged --quiet; then
+      echo "Nenhuma mudança para commit."
+      exit 0
+    fi
+
+    git commit -m "Generate Jekyll articles"
     git pull --rebase origin main
     git push origin main
